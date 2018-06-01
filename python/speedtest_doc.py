137db426ff2a884442d467ec71525574ff1262b1
MaxAvatar <maxavatar007@gmail.com>
2018-06-01 05:45:48 +0300

    Working tests

--- python/speedtest_doc.py
+++ python/speedtest_doc.py
@@ -1,12 +1,17 @@
+import sys
+import time
+
 from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import NUMERIC
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.expression import func
+from sqlalchemy.types import String
+
 from models.DocModels import Base
 from models.DocModels import Edge
 from models.DocModels import MetaVertex as Node
-import time
-from sqlalchemy.dialects.postgresql import NUMERIC
-from sqlalchemy.sql.expression import cast
+
+sys.setrecursionlimit(20000)
 
 engine = create_engine("postgresql+psycopg2://maxavatar:1234@185.98.87.154:5432/doc_mgraph", echo=False)
 Base.metadata.create_all(engine)
@@ -36,7 +41,7 @@ def add_remove_node(node_count=10):
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
 
-        print("+NODE id:{} --- {} seconds ---".format(n, time_delta))
+        # print("+NODE id:{} --- {} seconds ---".format(n, time_delta))
     print("+NODE {} --- AVG {} seconds ---".format(node_count, avg_time_total / avg_time_count))
 
     avg_time_total = 0
@@ -51,7 +56,7 @@ def add_remove_node(node_count=10):
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
 
-        print("-NODE id:{} --- {} seconds ---".format(max_id, time_delta))
+        # print("-NODE id:{} --- {} seconds ---".format(max_id, time_delta))
     print("-NODE {} --- AVG {} seconds ---".format(node_count, avg_time_total / avg_time_count))
 
     clear_db()
@@ -70,7 +75,8 @@ def add_remove_edge(edge_count=10):
         time_delta = time.time() - s
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
-        print("+EDGE id:{} --- {} seconds ---".format(n, time_delta))
+
+        # print("+EDGE id:{} --- {} seconds ---".format(n, time_delta))
     print("+EDGE {} --- AVG {} seconds ---".format(edge_count, avg_time_total / avg_time_count if avg_time_count > 0
     else 1))
 
@@ -86,7 +92,8 @@ def add_remove_edge(edge_count=10):
         time_delta = time.time() - s
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
-        print("-EDGE id:{} --- {} seconds ---".format(n, time_delta))
+
+        # print("-EDGE id:{} --- {} seconds ---".format(n, time_delta))
     print("-EDGE {} --- AVG {} seconds ---".format(edge_count, avg_time_total / avg_time_count if avg_time_count > 0
     else 1))
 
@@ -112,15 +119,191 @@ def edit_node(try_count=10):
         time_delta = time.time() - s
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
-        print("edit NODE try:{} --- {} seconds ---".format(n, time_delta))
+
+        # print("edit NODE try:{} --- {} seconds ---".format(n, time_delta))
     print("edit NODE {} --- AVG {} seconds ---".format(try_count, avg_time_total / avg_time_count))
 
     clear_db()
 
 
-if __name__ == "__main__":
-    # add_remove_node(100)
+def get_flat(node_count=10):
+    clear_db()
 
-    # add_remove_edge(100)
+    N = Node(data={'id': 0, 'parent': 0, 'value': 0})
+    session.add(N)
+    session.commit()
+    for a in range(node_count + 1):
+        a += 1
+        n = Node(data={'id': a, 'parent': 0, 'value': 0})
+        session.add(Edge(data={'from_id': 0, 'to_id': a, 'rel_type': 'rel', 'parent': 0, 'value': 0}))
+        session.add(n)
+        session.commit()
+
+    s = time.time()
+    items = session.query(Edge).filter(Edge.data["from_id"].astext.cast(NUMERIC) == 0
+                                       and Edge.data["rel_type"].astext.cast(String) == 'rel').all()
+    for item in items:
+        item = session.query(Node).filter(Node.data["id"].astext.cast(NUMERIC) == item.data['to_id']).one()
+    time_delta = time.time() - s
+    print("FOUND FLAT count:{} --- {} seconds ---".format(node_count, time_delta))
+
+    clear_db()
+
+
+def get_depth(node_count=10):
+    clear_db()
+
+    nodes = []
+
+    N = Node(data={'id': 0, 'parent': 0, 'value': 0})
+    nodes.append(N)
+    session.add(N)
+    session.commit()
+    for a in range(node_count):
+        a += 1
+        n = Node(data={'id': a, 'parent': 0, 'value': 0})
+        session.add(Edge(data={'from_id': nodes[-1].data['id'], 'to_id': n.data['id'], 'rel_type': 'rel', 'parent': 0,
+                               'value': 0}))
+        nodes.append(n)
+        session.add(n)
+        session.commit()
+
+    s = time.time()
+    item = find_recur(session.query(Edge).filter(Edge.data["from_id"].astext.cast(NUMERIC) == 0
+                                                 and Edge.data["rel_type"].astext.cast(String) == 'rel').all())
+    time_delta = time.time() - s
+    print("FOUND DEPTH count:{} --- {} seconds ---".format(node_count, time_delta))
+
+    clear_db()
+
+
+def del_depth(node_count=10):
+    clear_db()
+
+    nodes = []
+
+    N = Node(data={'id': 0, 'parent': 0, 'value': 0})
+    nodes.append(N)
+    session.add(N)
+    session.commit()
+    for a in range(node_count + 1):
+        a += 1
+        n = Node(data={'id': a, 'parent': 0, 'value': 0})
+        session.add(Edge(data={'from_id': nodes[-1].data['id'], 'to_id': n.data['id'], 'rel_type': 'rel', 'parent': 0,
+                               'value': 0}))
+        nodes.append(n)
+        session.add(n)
+        session.commit()
+
+    s = time.time()
+    del_recur(session.query(Edge).filter(Edge.data["from_id"].astext.cast(NUMERIC) == 0
+                                         and Edge.data["rel_type"].astext.cast(String) == 'rel').all())
+    time_delta = time.time() - s
+    print("DELETED DEPTH count:{} --- {} seconds ---".format(node_count, time_delta))
+
+    clear_db()
+
+
+def del_flat(node_count=10):
+    clear_db()
+
+    N = Node(data={'id': 0, 'parent': 0, 'value': 0})
+    session.add(N)
+    session.commit()
+    for a in range(node_count + 1):
+        a += 1
+        n = Node(data={'id': a, 'parent': 0, 'value': 0})
+        session.add(Edge(data={'from_id': 0, 'to_id': a, 'rel_type': 'rel', 'parent': 0, 'value': 0}))
+        session.add(n)
+        session.commit()
+
+    s = time.time()
+    items = session.query(Edge).filter(Edge.data["from_id"].astext.cast(NUMERIC) == 0
+                                       and Edge.data["rel_type"].astext.cast(String) == 'rel').all()
+    for item in items:
+        session.delete(session.query(Edge).filter(Edge.data["to_id"].astext.cast(NUMERIC) == item.data['to_id']
+                                                  and Edge.data["rel_type"].astext.cast(String) == 'rel').one())
+        session.delete(session.query(Node).filter(Node.data["id"].astext.cast(NUMERIC) == item.data['to_id']).one())
+        session.commit()
+    session.delete(session.query(Node).filter(Node.data["id"].astext.cast(NUMERIC) == 0).one())
+    session.commit()
+    time_delta = time.time() - s
+    print("DELETED FLAT count:{} --- {} seconds ---".format(node_count, time_delta))
+
+    clear_db()
+
+
+def find_recur(nodes):
+    for node in nodes:
+        a = session.query(Edge).filter(Edge.data["from_id"].astext.cast(NUMERIC) == node.data['to_id']
+                                       and Edge.data["rel_type"].astext.cast(String) == 'rel').all()
+
+        if len(a) == 0:
+            return node
+
+        return find_recur(a)
+
+
+def del_recur(nodes):
+    for node in nodes:
+        a = session.query(Edge).filter(Edge.data["from_id"].astext.cast(NUMERIC) == node.data['to_id']
+                                       and Edge.data["rel_type"].astext.cast(String) == 'rel').all()
+
+        if len(a) == 0:
+            session.delete(node)
+            session.delete(
+                session.query(Node).filter(Node.data["id"].astext.cast(NUMERIC) == node.data['from_id']).one())
+            session.delete(
+                session.query(Node).filter(Node.data["id"].astext.cast(NUMERIC) == node.data['to_id']).one())
+            session.commit()
+            return node
+
+        session.delete(node)
+        session.delete(session.query(Node).filter(Node.data["id"].astext.cast(NUMERIC) == node.data['from_id']).one())
+        session.commit()
+
+        return del_recur(a)
+
+
+if __name__ == "__main__":
+    add_remove_node(10)
+
+    add_remove_edge(10)
 
     edit_node(10)
+
+    get_flat(10)
+
+    get_flat(100)
+
+    get_flat(500)
+
+    get_flat(1000)
+
+    get_flat(5000)
+
+    get_flat(10000)
+
+    get_depth(10)
+
+    get_depth(100)
+
+    get_depth(500)
+
+    get_depth(1000)
+
+    get_depth(5000)
+
+    get_depth(10000)
+
+    del_depth(10)
+
+    del_depth(50)
+
+    del_depth(100)
+
+    del_flat(10)
+
+    del_flat(50)
+
+    del_flat(100)
--- python/speedtest_rel.py
+++ python/speedtest_rel.py
@@ -1,12 +1,15 @@
+import sys
+import time
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.expression import func
+
 from models.IdModels import Base
-from models.IdModels import Relation as Edge
 from models.IdModels import MetaVertex as Node
-import time
-from sqlalchemy.dialects.postgresql import NUMERIC
-from sqlalchemy.sql.expression import cast
+from models.IdModels import Relation as Edge
+
+sys.setrecursionlimit(20000)
 
 engine = create_engine("postgresql+psycopg2://maxavatar:1234@185.98.87.154:5432/id_mgraph", echo=False)
 Base.metadata.create_all(engine)
@@ -23,36 +26,37 @@ def clear_db():
 def add_remove_node(node_count=10):
     clear_db()
 
+    nodes = []
+
     avg_time_total = 0
     avg_time_count = 0
-    max_id = session.query(func.max(Node.node_id)).first()[0]
-    max_id = 1 if max_id is None else int(max_id) + 1
-    for n in range(max_id, max_id + node_count):
+    for n in range(node_count):
+        n = Node("v{}".format(n))
+        nodes.append(n)
         s = time.time()
-        session.add(Node("v{}".format(n)))
+        session.add(n)
         session.commit()
         time_delta = time.time() - s
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
 
-        print("+NODE id:{} --- {} seconds ---".format(n, time_delta))
+        # print("+NODE id:{} --- {} seconds ---".format(n, time_delta))
     print("+NODE {} --- AVG {} seconds ---".format(node_count, avg_time_total / avg_time_count))
 
     avg_time_total = 0
     avg_time_count = 0
 
-    max_id = session.query(func.max(Node.node_id)).first()[0]
+    max_id = session.query(func.max(Node.id)).first()[0]
     max_id = 1 if max_id is None else int(max_id) + 1
 
-    for n in range(node_count):
+    for n in nodes:
         s = time.time()
-        a = session.query(Node).filter(Node.node_id == max_id).one()
-        session.delete(a)
+        a = session.delete(n)
         session.commit()
         time_delta = time.time() - s
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
-        print("-NODE id:{} --- {} seconds ---".format(max_id, time_delta))
+        # print("-NODE id:{} --- {} seconds ---".format(max_id, time_delta))
 
         max_id -= 1
 
@@ -77,14 +81,14 @@ def add_remove_edge(edge_count=10):
     edges = []
     for n in range(len(nodes) - 1):
         s = time.time()
-        e = Edge(nodes[n], nodes[n])
+        e = Edge("", nodes[n], nodes[n + 1])
         edges.append(e)
         session.add(e)
         session.commit()
         time_delta = time.time() - s
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
-        print("+EDGE id:{} --- {} seconds ---".format(n, time_delta))
+        # print("+EDGE id:{} --- {} seconds ---".format(n, time_delta))
     print("+EDGE {} --- AVG {} seconds ---".format(edge_count, avg_time_total / avg_time_count))
 
     avg_time_total = 0
@@ -97,8 +101,8 @@ def add_remove_edge(edge_count=10):
         time_delta = time.time() - s
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
-        print("-EDGE id:{} --- {} seconds ---".format(i, time_delta))
-    print("-EDGE {} --- AVG {} seconds ---".format(edge_count, avg_time_total/avg_time_count))
+        # print("-EDGE id:{} --- {} seconds ---".format(i, time_delta))
+    print("-EDGE {} --- AVG {} seconds ---".format(edge_count, avg_time_total / avg_time_count))
 
     clear_db()
 
@@ -114,13 +118,13 @@ def edit_node(try_count=10):
 
     for n in range(try_count):
         s = time.time()
-        session.query(Node).filter(Node.name == "test_{}".format(n-1)).update({'name': "test_{}".format(n)})
+        session.query(Node).filter(Node.name == "test_{}".format(n - 1)).update({'name': "test_{}".format(n)})
         session.commit()
         time_delta = time.time() - s
         avg_time_total += time_delta
         avg_time_count += 0 if time_delta == 0 else 1
-        print("edit NODE id:{} --- {} seconds ---".format(n, time_delta))
-    print("edit NODE {} --- AVG {} seconds ---".format(try_count, avg_time_total/avg_time_count))
+        # print("edit NODE id:{} --- {} seconds ---".format(n, time_delta))
+    print("edit NODE {} --- AVG {} seconds ---".format(try_count, avg_time_total / avg_time_count))
 
     clear_db()
 
@@ -235,16 +239,44 @@ def del_recur(node):
 
 
 if __name__ == "__main__":
-    # add_remove_node(100)
+    add_remove_node(10)
 
-    # add_remove_edge(100)
+    add_remove_edge(10)
 
-    # edit_node(10)
+    edit_node(10)
 
-    # get_flat(1000)
+    get_flat(10)
 
-    # get_depth(100)
+    get_flat(100)
 
-    # del_depth(100)
+    get_flat(500)
+
+    get_flat(1000)
+
+    get_flat(5000)
+
+    get_flat(10000)
+
+    get_depth(10)
+
+    get_depth(100)
+
+    get_depth(500)
+
+    get_depth(1000)
+
+    get_depth(5000)
+
+    get_depth(10000)
+
+    del_depth(10)
+
+    del_depth(50)
+
+    del_depth(100)
+
+    del_flat(10)
+
+    del_flat(50)
 
     del_flat(100)
